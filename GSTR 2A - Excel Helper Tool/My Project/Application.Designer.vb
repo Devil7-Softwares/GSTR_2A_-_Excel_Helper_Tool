33a48017a9c3d78339716aae8678673bcad99b33
Devil7DK <dineshthangavel47@gmail.com>
2018-12-07 17:56:39 +0530

    Set Main Form

--- GSTR 2A - Excel Helper Tool/My Project/Application.Designer.vb
+++ GSTR 2A - Excel Helper Tool/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GSTR_2A___Excel_Helper_Tool.Form1
+            Me.MainForm = Global.GSTR_2A___Excel_Helper_Tool.frm_Main
         End Sub
     End Class
 End Namespace
